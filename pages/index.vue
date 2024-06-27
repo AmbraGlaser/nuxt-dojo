0df9b2b4b1cc3b82ad9d18fe0ef846b0f80dd0a3
ABNAmbr0 <120992662+ABNAmbr0@users.noreply.github.com>
2024-06-27 19:09:52 +0200

    nuxt-dojo

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, earum
+      odio totam omnis numquam molestiae! Aperiam, illo? Laborum, autem sed!
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, earum
+      odio totam omnis numquam molestiae! Aperiam, illo? Laborum, autem sed!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
